820694971b8ae87b5e04bae83c4b663b7d9853f9
bori00 <fazakasbori@gmail.com>
2023-01-06 03:25:01 +0200

    setup https for grpc

--- src/commons/hosts.js
+++ src/commons/hosts.js
@@ -1,4 +1,4 @@
 export const HOST = {
-    backend_api: 'http://localhost:8080', //http://energy-utility-backend-bori00.germanywestcentral.azurecontainer.io:8080',
-    grpc_api: 'http://localhost:8081'
+    backend_api: 'https://localhost:8443', //http://energy-utility-backend-bori00.germanywestcentral.azurecontainer.io:8080',
+    grpc_api: 'https://localhost:8081'
 };
